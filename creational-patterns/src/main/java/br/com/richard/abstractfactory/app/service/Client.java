c4fd1a4ebe4b65528ec0bb6b3defb6fd445ad7b9
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2023-09-29 21:10:35 -0300

    Abstract Factory

--- creational-patterns/src/main/java/br/com/richard/abstractfactory/app/service/Client.java
+++ creational-patterns/src/main/java/br/com/richard/abstractfactory/app/service/Client.java
@@ -1,6 +1,7 @@
 package br.com.richard.abstractfactory.app.service;
 
 import br.com.richard.abstractfactory.app.service.factory.EJBAbstractFactory;
+import br.com.richard.abstractfactory.app.service.factory.RestAbstractFactory;
 import br.com.richard.abstractfactory.app.service.factory.ServicesAbstractFactory;
 import br.com.richard.abstractfactory.app.service.services.CarService;
 import br.com.richard.abstractfactory.app.service.services.UserService;
@@ -8,15 +9,24 @@ import br.com.richard.abstractfactory.app.service.services.UserService;
 public class Client {
 
 	public static void main(String[] args) {
-		ServicesAbstractFactory factory = new EJBAbstractFactory();
-//		ServicesAbstractFactory factory = new RestAbstractFactory();
-		
-		UserService userService = factory.getUserService();
+		ServicesAbstractFactory ejbAbstractFactoryJ = new EJBAbstractFactory();
+
+		UserService userService = ejbAbstractFactoryJ.getUserService();
 		userService.save("Jhon");
 		userService.delete(5);
 		
-		CarService carService = factory.getCarService();
+		CarService carService = ejbAbstractFactoryJ.getCarService();
 		carService.save("Prius");
 		carService.update("Tesla X");
+
+		ServicesAbstractFactory restAbstractFactory = new RestAbstractFactory();
+
+		UserService userServiceRest = restAbstractFactory.getUserService();
+		userServiceRest.save("Richard");
+		userServiceRest.delete(3);
+
+		CarService carServiceRest = restAbstractFactory.getCarService();
+		carServiceRest.save("Jeep Compass");
+		carServiceRest.update("RamPage");
 	}
 }
